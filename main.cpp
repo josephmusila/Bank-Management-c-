2e371ad8780fb229c3c6ac2cde96c0836aaf2b38
josephmusila <josemusila03@gmail.com>
2021-01-17 16:03:27 -0800

    added deposit

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <fstream>
 #include<cstdlib>
 
-
+using std::fstream;
+using std::ofstream;
+using std::ifstream;
 using namespace std;
 
 class Bank_rec{
@@ -19,35 +21,72 @@ private:
 public:
     void register_account();
     void deposit_cash();
+    void add_deposits();
     void withdraw_cash();
     void check_balance();
     void write_rec();
     void show_rec();
     void read_rec();
 
+
 };
 void Bank_rec::register_account(){
-    cout<<"Enter your name: ";
+
+
+        cout<<"Enter your name: ";
     cin>>firstName;
     cout<<"Enter your Last Name: ";
     cin>>lastName;
     cout<<"Enter your Id Number: ";
     cin>>idNumber;
-
-    withdrawal=0.00;
-    balance=0.00;
     deposit=0.00;
-    accountNumber=rand();
+    balance=0.00;
+    withdrawal=0.00;
+
+
+    int choice;
+    int idNum=(int)idNumber;
+    accountNumber=rand()+10000;
     cout<<"---------------------------------"<<endl;
-    cout<<"Your account Number is: "<<idNumber<<accountNumber<<endl;
+    cout<<"Your account Number is: "<<accountNumber<<endl;
     cout<<"---------------------------------"<<endl;
+    cout<<" Do you Wish to Deposit Any amount ? "<<endl;
+    cout<<"\t1-->Yes"<<endl;
+    cout<<"\t2--No"<<endl;
+    cin>>choice;
+
+
+
+    switch(choice)
+    {
+    case 1:
+        cout<<"Enter the Amount To Deposit: ";
+        cin>>deposit;
+        balance=deposit;
+         cout<<"Your new balance is: "<<balance<<endl;
+        show_rec();
+         cout<<"Dear "<<firstName<<" Thank You for Registering with us\n"<<endl;
+       cout<<"_________________________________________________________"<<endl;
+        break;
+    case 2:
+        show_rec();
+        cout<<"Dear "<<firstName<<" Thank You for Registering with us\n"<<endl;
+       cout<<"_________________________________________________________"<<endl;
+
+        break;
+    default:
+        exit(0);
+    }
+
+
 
 }
 void Bank_rec::show_rec(){
+
     cout<<"First Name: "<<firstName<<endl;
     cout<<"Last Name: "<<lastName<<endl;
     cout<<"Id Number: "<<idNumber<<endl;
-    cout<<"Account Number: "<<idNumber<<accountNumber<<endl;
+    cout<<"Account Number: "<<accountNumber<<endl;
     cout<<"Withdrawals: "<<withdrawal<<endl;
     cout<<"Balance: "<<balance<<endl;
     cout<<"Deposits: "<<deposit<<endl;
@@ -80,19 +119,58 @@ void Bank_rec::read_rec(){
 
 }
 
+void Bank_rec::add_deposits(){
+
+    cout<<"Enter amount to Deposit: ";
+    cin>>deposit;
+    deposit=deposit;
+    balance=deposit;
+    show_rec();
+
+}
+void Bank_rec::deposit_cash(){
+    int n;
+    fstream iofile;
+    iofile.open("records.txt",ios::binary|ios::in);
+    if(!iofile)
+    {
+        cout<<"Error openning file "<<endl;
+        return;
+    }
+    iofile.seekg(0,ios::end);
+    int count=iofile.tellg()/sizeof(*this);
+    cout<<"Deposits Procedure"<<endl;
+    cout<<"___________________________\n\n"<<endl;
+    cout<<"Enter your AccountNo: ";
+    cin>>n;
+    iofile.seekg((n-1)*sizeof(*this));
+    iofile.read(reinterpret_cast<char*>(this),sizeof(*this));
+    cout<<"Records of Account "<<n<<endl;
+    show_rec();
+    iofile.close();
+    iofile.open("records.txt",ios::out|ios::binary|ios::in);
+    iofile.read(reinterpret_cast<char*>(this),sizeof(*this));
+    add_deposits();
+    iofile.read(reinterpret_cast<char*>(this),sizeof(*this));
+
+
+
+}
 int main()
 {
     Bank_rec A;
     int choice;
-
+    cout<<"\n\t\t\t **********KCB KENYA TERMINAL SERVICES*********"<<endl;
+    cout<<"\t ---------------------------------------------------------------------\n\n";
+    cout<<"\t\t\tSelect your desired service\n"<<endl;
     while(true)
     {
-            cout<<"\n **********KCB KENYA TERMINAL SERVICES*********"<<endl;
-    cout<<"Select your desired service"<<endl;
+
     cout<<"\t1 -->Register for an acount"<<endl;
     cout<<"\t2 -->Exit "<<endl;
     cout<<"\t3 -->Show all records"<<endl;
-    cout<<"\n Enter your choice"<<endl;
+    cout<<"\t4 -->Deposit Cash"<<endl;
+    cout<<"\t\n Enter your choice"<<endl;
     cin>>choice;
 
 
@@ -107,9 +185,14 @@ int main()
         case 3:
             A.read_rec();
             break;
+        case 4:
+            A.deposit_cash();
+            break;
         default:
-            cout<<"You Entered invalid Choice"<<endl;
-            exit(0);
+            cout<<"--------------------------------"<<endl;
+            cout<<"You Entered invalid Choice!!"<<endl;
+            cout<<"--------------------------------"<<endl;
+
         }
     }
     system("pause");
